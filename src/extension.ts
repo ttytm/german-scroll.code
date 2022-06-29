58a272507c7e57a2b183f0528e3ad96a3cf4f99a
tobealive <tobmoj@web.de>
2022-06-29 22:46:00 +0200

    Cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -9,50 +9,46 @@ const scroll = (direction: ScrollDirection, distance: ScrollDistance) => {
    if (!editor) return;
 
    const { visibleRanges } = editor,
-      firstSegmentRangeStart = visibleRanges[0].start.line,
-      firstSegmentRangeEnd = visibleRanges[0].end.line,
-      lastSegmentRangeStart = visibleRanges[visibleRanges.length - 1].start.line,
-      lastSegmentRangeEnd = visibleRanges[visibleRanges.length - 1].end.line,
-      allSegmentsRanges = calculateRanges(direction),
-      allSegmentsRangeValue = allSegmentsRanges.end - allSegmentsRanges.start,
+      ranges = calculateRanges(direction),
+      rangeValue = ranges.end - ranges.start,
 
       cursorPosition = editor.selection.active.line,
-      firstSegmentDistanceStart = cursorPosition - firstSegmentRangeStart,
-      firstSegmentDistanceEnd = firstSegmentRangeEnd - cursorPosition,
-      lastSegmentDistanceStart = cursorPosition - lastSegmentRangeStart,
-      lastSegmentDistanceEnd = lastSegmentRangeEnd - cursorPosition,
+      firstSegmentDistanceStart = cursorPosition - ranges.start,
+      firstSegmentDistanceEnd = visibleRanges[0].end.line - cursorPosition,
+      lastSegmentDistanceStart = cursorPosition - visibleRanges[visibleRanges.length - 1].start.line,
+      lastSegmentDistanceEnd = ranges.end - cursorPosition,
 
       hasNumberDistance = distance !== 'halfPage' && distance !== 'page',
-      hasFolds = firstSegmentDistanceStart < 0 || firstSegmentDistanceEnd < 0 || (firstSegmentDistanceEnd <= scrollOff && allSegmentsRanges.end !== firstSegmentRangeEnd),
+      hasFolds = firstSegmentDistanceStart < 0 || firstSegmentDistanceEnd < 0 || (firstSegmentDistanceEnd <= scrollOff && ranges.end !== visibleRanges[0].end.line),
       hasScrollOffContact = (hasNumberDistance && (direction === 'down' && scrollOff >= firstSegmentDistanceStart))
          || (hasNumberDistance && (direction === 'up' && scrollOff + 1 >= lastSegmentDistanceEnd));
 
-   let distanceValue = distance === 'halfPage' ? Math.floor(allSegmentsRangeValue / 2) : distance === 'page' ? allSegmentsRangeValue : distance;
+   let distanceValue = distance === 'halfPage' ? Math.floor(rangeValue / 2) : distance === 'page' ? rangeValue : distance;
 
    // Fix cursor moves to end of line when scrolling beyond last line
-   if (hasNumberDistance && direction === 'down' && allSegmentsRangeValue <= scrollOff + distance) {
+   if (hasNumberDistance && direction === 'down' && rangeValue <= scrollOff + distance) {
       console.log('#fix');
-      moveViewport(direction, allSegmentsRangeValue - scrollOff);
-      moveCursor(direction, allSegmentsRangeValue - scrollOff);
+      moveViewport(direction, rangeValue - scrollOff);
+      moveCursor(direction, rangeValue - scrollOff);
       // vscode.commands.executeCommand('cursorMove', { to: 'viewPortBottom', by: 'wrappedLine' });
    }
    // Keep cursor moving when top boundary is reached
-   else if (firstSegmentRangeStart === 0 && direction === 'up') {
+   else if (visibleRanges[0].start.line === 0 && direction === 'up') {
       console.log('#moveCurosor');
       moveViewport(direction, distanceValue);
       moveCursor(direction, distanceValue);
    }
    // Scroll from top boundary 
-   else if (hasNumberDistance && firstSegmentRangeStart === 0 && direction === 'down') {
+   else if (hasNumberDistance && visibleRanges[0].start.line === 0 && direction === 'down') {
       console.log('#fromTop');
       moveViewport(direction, distance + scrollOff);
       alignViewport('up');
    }
    // Scroll when lines to top boundary remain
-   else if (firstSegmentRangeStart > 0 && firstSegmentRangeStart <= scrollOff && direction === 'up') {
+   else if (visibleRanges[0].start.line > 0 && visibleRanges[0].start.line <= scrollOff && direction === 'up') {
       console.log('#remainingLines');
-      moveViewport(direction, allSegmentsRanges.start);
-      moveCursor(direction, allSegmentsRanges.start);
+      moveViewport(direction, ranges.start);
+      moveCursor(direction, ranges.start);
    }
    // Scroll when visibleRange is mutated through folds
    else if (hasNumberDistance && (hasFolds)) {
