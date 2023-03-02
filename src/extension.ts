b64103bf04d197553f1504f8eb3c6174522e5b7d
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-02 11:37:26 +0100

    refact: prepare for v0.2.0 release

--- src/extension.ts
+++ src/extension.ts
@@ -1,96 +1,48 @@
 import * as vscode from "vscode";
-import { moveViewport, moveCursor, alignViewport, calculateRanges } from "./utils";
+import { moveViewport, moveCursor, alignViewport, calcVisibleLines } from "./utils";
 import { ScrollDirection, ScrollDistance, Scroller, SCROLLERS } from "./types";
 
 let scrollOff = vscode.workspace.getConfiguration("editor").get("cursorSurroundingLines") as number;
 
-const scroll = (direction: ScrollDirection, distance: ScrollDistance) => {
+const scroll = (scroller: Scroller, direction: ScrollDirection, distance: ScrollDistance) => {
 	const editor = vscode.window.activeTextEditor!;
-	if (!editor) return;
+	const ranges = editor.visibleRanges; // including potential folds
+	const visibleLines = calcVisibleLines(ranges);
+	let scrollDistance = distance === "halfPage" ? visibleLines / 2 : distance === "page" ? visibleLines : distance;
 
-	// viewport
-	const { visibleRanges } = editor,
-		ranges = calculateRanges(direction),
-		rangeValue = ranges.end - ranges.start,
-		// positional
-		cursorPosition = editor.selection.active.line,
-		firstSegmentDistanceStart = cursorPosition - ranges.start,
-		firstSegmentDistanceEnd = visibleRanges[0].end.line - cursorPosition,
-		lastSegmentDistanceStart = cursorPosition - visibleRanges[visibleRanges.length - 1].start.line,
-		lastSegmentDistanceEnd = ranges.end - cursorPosition,
-		// conditional
-		hasNumberDistance = typeof distance === "number",
-		hasFolds =
-			firstSegmentDistanceStart < 0 ||
-			firstSegmentDistanceEnd < 0 ||
-			(firstSegmentDistanceEnd <= scrollOff && ranges.end !== visibleRanges[0].end.line),
-		hasScrollOffContact =
-			(hasNumberDistance && direction === "down" && scrollOff >= firstSegmentDistanceStart) ||
-			(hasNumberDistance && direction === "up" && scrollOff + 1 >= lastSegmentDistanceEnd);
-
-	let distanceValue =
-		distance === "halfPage"
-			? Math.floor(rangeValue / 2)
-			: distance === "page"
-			? rangeValue
-			: distance;
-
-	// Fix cursor moves to end of line when scrolling beyond last line
-	if (hasNumberDistance && direction === "down" && rangeValue <= scrollOff + distance) {
-		console.log("%cextension.ts line:40 beyondlast", "color: #007acc;");
-		moveViewport(direction, rangeValue - scrollOff);
-		moveCursor(direction, rangeValue - scrollOff);
-		// vscode.commands.executeCommand('cursorMove', { to: 'viewPortBottom', by: 'wrappedLine' });
-	}
-	// Keep cursor moving when top boundary is reached
-	else if (visibleRanges[0].start.line === 0 && direction === "up") {
-		moveViewport(direction, distanceValue);
-		moveCursor(direction, distanceValue);
-	}
-	// Scroll from top boundary
-	else if (hasNumberDistance && visibleRanges[0].start.line === 0 && direction === "down") {
-		moveViewport(direction, distance + scrollOff);
-		alignViewport("up");
-	}
-	// Scroll when lines to top boundary remain
-	else if (
-		visibleRanges[0].start.line > 0 &&
-		visibleRanges[0].start.line <= scrollOff &&
-		direction === "up"
-	) {
-		moveViewport(direction, ranges.start);
-		moveCursor(direction, ranges.start);
-	}
-	// Scroll when visibleRange is mutated through folds
-	else if (hasNumberDistance && hasFolds) {
-		const hasFoldBelow = lastSegmentDistanceStart <= scrollOff,
-			hasFoldAbove = firstSegmentDistanceEnd <= scrollOff;
-
-		distanceValue =
-			(direction === "down" && hasFoldBelow) || (direction === "up" && hasFoldAbove)
-				? distance + scrollOff
-				: distance;
-
-		moveViewport(direction, distanceValue);
-		alignViewport(direction);
-	}
-	// Scroll when cursor is touching scrollOff
-	else if (hasScrollOffContact) {
-		moveViewport(direction, distance);
-		moveCursor(direction, distance < scrollOff ? distance : scrollOff);
-	}
-	// Scroll when using string distance values
-	else if (!hasNumberDistance) {
-		vscode.commands.executeCommand("editorScroll", {
-			to: direction,
-			by: distance,
-			revealCursor: true,
-		});
-		alignViewport();
-	}
-	// Scroll default
-	else {
-		moveViewport(direction, distance);
+	switch (true) {
+		// Keep cursor moving when top boundary is reached
+		case ranges[0].start.line === 0 && direction === "up":
+			moveViewport(direction, scrollDistance);
+			moveCursor(direction, scrollDistance);
+			break;
+		// Scroll from top boundary
+		case ranges[0].start.line === 0 && direction === "down":
+			moveViewport(direction, scrollDistance + scrollOff);
+			moveCursor(direction, scrollDistance);
+			break;
+		// Simulating Neovims behavior, ctrl+e / ctrl+y favors scrolling the viewport solely, instead of moving the cursor simultaneously
+		case scroller === "armin":
+			moveViewport(direction, scrollDistance, "revealCursor");
+			const cursorPosition = editor.selection.active.line;
+			const hasScrollOffContact =
+				(direction === "down" && scrollOff >= cursorPosition - ranges[0].start.line - 1) ||
+				(direction === "up" && scrollOff >= ranges[ranges.length - 1].end.line - cursorPosition);
+			const hasFoldAbove = ranges[0].end.line - cursorPosition <= scrollOff,
+				hasFoldBelow = cursorPosition - ranges[ranges.length - 1].start.line <= scrollOff;
+			if (hasScrollOffContact) {
+				moveCursor(direction, scrollOff < scrollDistance ? scrollOff : scrollDistance);
+				return;
+			}
+			if ((direction === "down" && hasFoldBelow) || (direction === "up" && hasFoldAbove)) {
+				return;
+			}
+			alignViewport();
+			break;
+		default:
+			moveViewport(direction, scrollDistance);
+			moveCursor(direction, scrollDistance);
+			break;
 	}
 };
 
@@ -104,15 +56,13 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.workspace.onDidChangeConfiguration(async (event) => {
 			if (event.affectsConfiguration("germanScroll")) config = getConfig(scroller);
 			if (event.affectsConfiguration("editor.cursorSurroundingLines")) {
-				scrollOff = vscode.workspace
-					.getConfiguration("editor")
-					.get("cursorSurroundingLines") as number;
+				scrollOff = vscode.workspace.getConfiguration("editor").get("cursorSurroundingLines") as number;
 			}
 		});
 
 		context.subscriptions.push(
-			vscode.commands.registerCommand(`germanScroll.${scroller}Down`, () => scroll("down", config)),
-			vscode.commands.registerCommand(`germanScroll.${scroller}Up`, () => scroll("up", config))
+			vscode.commands.registerCommand(`germanScroll.${scroller}Down`, () => scroll(scroller, "down", config)),
+			vscode.commands.registerCommand(`germanScroll.${scroller}Up`, () => scroll(scroller, "up", config))
 		);
 	});
 }
