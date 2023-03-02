3b340c3710151c6b9f057d468162878dc5e4c2fc
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-02 11:37:26 +0100

    refact: prepare for v0.2.0 release

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,33 @@
 import * as vscode from "vscode";
 import { ScrollDirection, ScrollDistance } from "./types";
 
-export const moveViewport = (direction: ScrollDirection, distance: ScrollDistance) => {
+const calcVisibleLines = (ranges: readonly vscode.Range[]): number => {
+	// viewportBottomLine - viewportTopLine
+	const linesInViewport = ranges[ranges.length - 1].end.line - ranges[0].start.line;
+	if (ranges.length === 0) return linesInViewport;
+
+	const visibleLines = ranges.reduce((acc, range) => {
+		return acc + range.end.line - range.start.line + 1;
+	}, 0);
+
+	return visibleLines - 1;
+};
+
+const moveViewport = (direction: ScrollDirection, distance: ScrollDistance, revealCursor?: "revealCursor") => {
 	vscode.commands.executeCommand("editorScroll", {
 		to: direction,
 		by: "wrappedLine",
 		value: distance,
-		revealCursor: true,
+		// `revealCursor` stabilizes armin around folds when touching scrolloff.
+		// Though, brings especially christa out of sync .
+		//
+		// Sizes of folded sections, smooth scrolling, configured scrollDistance, play into this.
+		// The perfect recipy is yet to be made.
+		revealCursor: revealCursor ? true : false,
 	});
 };
 
-export const moveCursor = (direction: ScrollDirection, distance: ScrollDistance) => {
+const moveCursor = (direction: ScrollDirection, distance: ScrollDistance) => {
 	vscode.commands.executeCommand("cursorMove", {
 		to: direction,
 		by: "wrappedLine",
@@ -18,7 +35,7 @@ export const moveCursor = (direction: ScrollDirection, distance: ScrollDistance)
 	});
 };
 
-export const alignViewport = (direction?: ScrollDirection) => {
+const alignViewport = (direction?: ScrollDirection) => {
 	if (direction) {
 		vscode.commands.executeCommand("cursorMove", { to: direction, by: "wrappedLine" });
 		return;
@@ -27,65 +44,4 @@ export const alignViewport = (direction?: ScrollDirection) => {
 	vscode.commands.executeCommand("cursorMove", { to: "down", by: "wrappedLine" });
 };
 
-// Credits to @bmalehorn for this calcs
-export const calculateRanges = (direction: ScrollDirection): { start: number; end: number } => {
-	const editor = vscode.window.activeTextEditor!;
-	const { visibleRanges } = editor;
-
-	if (direction === "down") {
-		const end = Math.min(
-			visibleRanges[visibleRanges.length - 1].end.line,
-			editor.document.lineCount
-		);
-
-		let start = visibleRanges[0].start.line;
-		let linesRemaining = end - visibleRanges[visibleRanges.length - 1].end.line;
-		let i = 0;
-
-		while (linesRemaining > 0 && i < visibleRanges.length) {
-			const range = visibleRanges[i];
-			const toBurn = Math.min(range.end.line - start, linesRemaining);
-			linesRemaining -= toBurn;
-			start += toBurn;
-			i++;
-			// burn 1 line to reach next visible range
-			if (linesRemaining > 0 && i < visibleRanges.length) {
-				linesRemaining--;
-				start = visibleRanges[i].start.line;
-			}
-		}
-		start += linesRemaining;
-
-		return { start, end };
-	} else if (direction === "up") {
-		// 1. top <=> bottom
-		// 2. 0 ... visibleRanges.length <=> visibleRanges.length ... 0
-		// 3. + <=> -
-		// 4. start <=> end
-		// it's probably possible to use the same code for up & down,
-		// but I'd rather have it inlined, out in the open
-		const start = Math.max(visibleRanges[0].start.line);
-
-		let end = visibleRanges[visibleRanges.length - 1].end.line;
-		let linesRemaining = visibleRanges[0].start.line - start;
-		let i = visibleRanges.length - 1;
-
-		while (linesRemaining > 0 && i >= 0) {
-			const range = visibleRanges[i];
-			const toBurn = Math.min(end - range.start.line, linesRemaining);
-			linesRemaining -= toBurn;
-			end -= toBurn;
-			i--;
-			// burn 1 line to reach next visible range
-			if (linesRemaining > 0 && i >= 0) {
-				linesRemaining--;
-				end = visibleRanges[i].end.line;
-			}
-		}
-		end -= linesRemaining;
-
-		return { start, end };
-	}
-
-	return { start: 0, end: 0 };
-};
+export { calcVisibleLines, moveViewport, moveCursor, alignViewport };
